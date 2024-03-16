79e92a86d4e3111d4ab04fb2804fab3ae2b6617b
t4ccer <t4ccer@gmail.com>
2024-03-15 21:27:32 -0600

    justshow_x11: Add user defined atom test

--- crates/justshow_x11/tests/atoms.rs
+++ crates/justshow_x11/tests/atoms.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use justshow_x11::{
     atoms::AtomId,
     replies::{self, String8},
@@ -7,6 +5,7 @@ use justshow_x11::{
     xerror::SomeError,
     XDisplay,
 };
+use std::str::FromStr;
 
 #[test]
 fn get_predefined_atom() {
@@ -24,6 +23,39 @@ fn get_predefined_atom() {
             name: String8::from_str("SUPERSCRIPT_X").unwrap()
         }
     )
+}
+
+#[test]
+fn get_user_defined_atom() {
+    let mut display = XDisplay::open().unwrap();
+
+    let atom_name = String8::from_str("I_AM_USER_DEFINED").unwrap();
+
+    let pending_intern = display
+        .send_request(&requests::InternAtom {
+            name: atom_name.clone(),
+            only_if_exists: false,
+        })
+        .unwrap();
+    let intern = display
+        .await_pending_reply(pending_intern)
+        .unwrap()
+        .unwrap();
+
+    let pending_get_name = display
+        .send_request(&requests::GetAtomName { atom: intern.atom })
+        .unwrap();
+    let get_name = display
+        .await_pending_reply(pending_get_name)
+        .unwrap()
+        .unwrap();
+
+    assert_eq!(
+        get_name,
+        replies::GetAtomName {
+            name: atom_name.clone()
+        }
+    );
 }
 
 #[test]
